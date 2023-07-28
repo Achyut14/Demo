9e99ae3c3261437d666b9c706204bb69fa47b3f1
Achyut Dahal <achutdahal14@gmail.com>
2023-07-28 13:37:01 -0400

    python ds practice

---
+++ Python/python-ds-practice/11_flip_case/flip_case.py
@@ -0,0 +1,15 @@
+def flip_case(phrase, to_swap):
+    """Flip [to_swap] case each time it appears in phrase.
+
+        >>> flip_case('Aaaahhh', 'a')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'A')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'h')
+        'AaaaHHH'
+
+    """
+    return ''.join(c.upper() if c.lower()==to_swap.lower() and c.isalpha() else c for c in phrase)
+
