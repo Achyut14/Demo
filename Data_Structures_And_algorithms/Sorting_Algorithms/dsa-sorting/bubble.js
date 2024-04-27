53b5a3c20db150d6cf2f840b309dada53317a99d
Achyut Dahal <achutdahal14@gmail.com>
2024-04-27 16:38:06 -0400

    Sorting Algorithms practice

---
+++ Data_Structures_And_algorithms/Sorting_Algorithms/dsa-sorting/bubble.js
@@ -0,0 +1,17 @@
+function bubbleSort(arr) {
+    let n = arr.length;
+    let swapped;
+    do{
+        swapped = false;
+        for (let i =1; i<n; i++){
+            if(arr[i -1]> arr[i]){
+                [arr[i-1], arr[i]] = [arr[i], arr[i-1]];
+                swapped = true;
+            }
+        }
+        n--;
+    } while (swapped);
+    return arr;
+}
+
+module.exports = bubbleSort;
